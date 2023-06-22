4c76180952794831950944d508e67e63fd247060
Shrinidhi bharadhwaj <54413312+snbharadhwaj@users.noreply.github.com>
2023-06-22 15:02:47 +0530

    Add files via upload

---
+++ Airbus/Fabricator/js/items.js
@@ -0,0 +1,78 @@
+var fabrication=[
+    {
+        "item": "Tub",
+        "raw_material": "Sheet Steel",
+        "Quantity": "10 sqft"
+    },
+    {
+        "item": "Pump",
+        "raw_material": "Plastics",
+        "Quantity": "10 kg"
+    },
+    {
+        "item": "Spin Tub",
+        "raw_material": "Stainless Steel",
+        "Quantity": "5 kg/m3"
+    },
+    {
+        "item": "Wash Tub",
+        "raw_material": "(enameling iron) Porcelain coating",
+        "Quantity": "24 gauge"
+    },
+    {
+        "item": "Balance Ring",
+        "raw_material": "Plastics",
+        "Quantity": "3 kg"
+    },
+    {
+        "item": "Transmission Gears",
+        "raw_material": "Cast Aluminum",
+        "Quantity": "ingots—20"
+    },
+    {
+        "item": "Plastic Brackets",
+        "raw_material": "Plastics",
+        "Quantity": "2 kg"
+    },
+    {
+        "item": "Spin Tub",
+        "raw_material": "Steel (enameling iron) Porcelain coating",
+        "Quantity": "5 kg/m3"
+    },
+    {
+        "item": "Blance Ring",
+        "raw_material": "Stainless Steel",
+        "Quantity": "2 kg/m3 "
+    },
+    {
+        "item": "Balance Ring",
+        "raw_material": "Porcelain Enamel",
+        "Quantity": "8 gauge"
+    },
+    {
+        "item": "Plastic Brackets",
+        "raw_material": "Plastics",
+        "Quantity": "3 kg"
+    },
+    {
+      "item": "Bearings",
+      "raw_material": "Stainless Steel",
+      "Quantity": "10 pieces"
+    },
+    {
+        "item": "Valve",
+        "raw_material": "Aluminum",
+        "Quantity": "1 piece"
+    },
+    {
+        "item": "Pneumatic Cylinder",
+        "raw_material": "Stainless Steel",
+        "Quantity": "20 kg"
+    },
+    {
+        "item": "Hydraulic Pump",
+        "raw_material": "Cast Iron",
+        "Quantity": "10 kg"
+    },
+    
+]
